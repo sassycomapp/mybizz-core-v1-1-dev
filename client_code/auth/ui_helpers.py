e87d223fe06afad0aa24ff072b398ceca1bc1968
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-01-04 09:53:02 +0000

    Created auth.LoginForm, auth.PasswordResetForm, auth.SignupForm, auth.ui_helpers and 31 more

---
+++ client_code/auth/ui_helpers.py
@@ -0,0 +1,18 @@
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from ..auth import Module1
+#
+#    Module1.say_hello()
+#
+
+
+def say_hello():
+  print("Hello, world")
---
+++ client_code/products/Variantmanager/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import VariantmanagerTemplate
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import stripe.checkout
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Variantmanager(VariantmanagerTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
