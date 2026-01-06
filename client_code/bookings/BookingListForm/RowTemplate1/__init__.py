6a9483dda3c0e4e5baedc5f72b0e88295c3cd2b2
sassycomapp@gmail.com <sassycomapp@gmail.com>
2026-01-06 16:51:35 +0000

    Created bookings.BookingListForm.RowTemplate1; Edited bookings.BookingListForm

--- client_code/bookings/BookingListForm/RowTemplate1/__init__.py
+++ client_code/bookings/BookingListForm/RowTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import BookingListFormTemplate
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -10,19 +10,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class BookingListForm(BookingListFormTemplate):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  @handle("btn_new_booking", "click")
-  def btn_new_booking_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
-
-  @handle("dp_date_to", "change")
-  def dp_date_to_change(self, **event_args):
-    """This method is called when the selected date changes"""
-    pass
